5418ef2eef74da6db990fabb8c0e07b48649ca5d
AlenaSazykina <443as.learn.ac@gmail.com>
2025-07-26 20:46:16 +0300

    Added Tasks 1-10 of javaclassandmethod

---
+++ src/main/java/org/example/javacircul/Task1.java
@@ -0,0 +1,22 @@
+package org.example.javacircul;
+/*
+Практика Java. Циклы #1
+Числа подряд. Дана следующая сигнатура метода:
+public static String listNums(int x);
+Необходимо реализовать метод таким образом, чтобы он возвращал строку, в которой будут записаны все числа от 0 до x (включительно). x может принимать значения от 0 и выше.
+Пример:
+x=5
+результат: “0 1 2 3 4 5”
+ */
+public class Task1 {
+    public static void main(String[] arg){
+        System.out.println(listNums(5));
+    }
+    public static String listNums(int x){
+        String res = "";
+        for(int i=0; i<=x; i++){
+            res+=i +" ";
+        }
+        return res;
+    }
+}
---
+++ src/main/java/org/example/javacircul/Task3.java
@@ -0,0 +1,24 @@
+package org.example.javacircul;
+/*
+Практика Java. Циклы #3
+Четные числа. Дана следующая сигнатура метода:
+public static String chet(int x);
+Необходимо реализовать метод таким образом, чтобы он возвращал строку, 
+в которой будут записаны все четные числа от 0 до x (включительно). 
+Подсказа для обеспечения качества кода: инструкцию if использовать не следует.
+Пример:
+x=9
+результат: “0 2 4 6 8”
+ */
+public class Task3 {
+    public static void main (String[] arg){
+        System.out.println(chet(9));
+    }
+    public static String chet(int x){
+        String res = "";
+        for(int i=0; i<=x; i+=2){
+            res+= i + " ";
+        }
+        return res;
+    }
+}
