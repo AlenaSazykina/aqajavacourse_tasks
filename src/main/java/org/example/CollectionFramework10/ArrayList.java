fec695dd475ccab9103ccfec7d4adde33612dfc6
Alena Sazykina <443as.learn.ac@gmail.com>
2025-09-22 08:40:55 +0300

    Changed GoogleSearch

---
+++ src/main/java/org/example/CollectionFramework10/ArrayList.java
@@ -0,0 +1,21 @@
+/*
+Задача #3. Замена массива на ArrayList
+Перед вами код метода, который переставляет числа в массиве в обратном порядке.
+
+public static void reverse(int[] intArray) {
+        int n = intArray.length - 1;
+        for (int i = 0; i < intArray.length / 2; i++) {
+            int temp = intArray[i];
+            intArray[i] = intArray[n - i];
+            intArray[n - i] = temp;
+        }
+    }
+Задача: написать такой же метод для ArrayList<Integer>. 
+ */
+
+package org.example.CollectionFramework10;
+
+
+public class ArrayList {
+    
+}
