b0e609ff475bcdedc94dbe2702b3ac50f0ca277c
Alena Sazykina <443as.learn.ac@gmail.com>
2025-09-29 23:20:15 +0300

    Added APT9Exception

---
+++ src/main/java/org/example/APT9Exception/Sum1.java
@@ -0,0 +1,18 @@
+package org.example.APT9Exception;
+
+public class Sum1 {
+
+    public static double sumStrings(String[] args) {
+    double sum = 0.0;
+    String numberPattern = "^[-+]?\\d*(\\.\\d+)?$"; // простое число, включая знаки +/-
+
+    for (String arg : args) {
+        if (arg != null && arg.matches(numberPattern)) {
+            sum += Double.parseDouble(arg);
+        } else {
+            
+        }
+    }
+    return sum;
+}
+}
